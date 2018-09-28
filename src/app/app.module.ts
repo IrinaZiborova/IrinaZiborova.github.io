4f9e4a25cec65f47a88e9fd3e37d5805b1329bbe
IrinaZiborova <irisha.ziborova@gmail.com>
2018-09-28 22:16:40 +0200

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NgxPopper } from 'angular-popper';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule,
+    NgbModule.forRoot(),
+    NgxPopper,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
+
 })
 export class AppModule { }
+export class OtherModule { }
